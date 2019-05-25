b92a147234d2fb93ee0bc6e9bb618d178f06f3c4
Brian Hedberg <bshslowfood@gmail.com>
2019-05-25 15:54:54 -0500

    remove startes

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,32 +1,30 @@
 const colors = require("../../src/styles/colors");
 
 module.exports = {
-  siteTitle: "Brian Shea Hedberg's Personal Site", // <title>
-  shortSiteTitle: "Brian Hedberg's Personal Site", // <title> ending for posts and pages
-  siteDescription: "Thoughts, feelings, ponderings",
-  siteUrl: "https://briansheahedberg.com",
-  pathPrefix: "",
-  siteImage: "preview.jpg",
-  siteLanguage: "en",
-  // author
-  authorName: "brian shea hedberg",
-  authorTwitterAccount: "greglobinski",
-  // info
-  infoTitle: "brian shea hedberg",
-  infoTitleNote: "personal site",
-  // manifest.json
-  manifestName: "Personal Site - Brian Hedberg",
-  manifestShortName: "Personal Site", // max 12 characters
-  manifestStartUrl: "/",
-  manifestBackgroundColor: colors.background,
-  manifestThemeColor: colors.background,
-  manifestDisplay: "standalone",
-  // contact
-  contactEmail: "briansheahedberg@gmail.com",
-  // social
-  authorSocialLinks: [
-    { name: "github", url: "https://github.com/gemfarmer" },
-    // { name: "twitter", url: "https://twitter.com/greglobinski" },
-    // { name: "facebook", url: "https://facebook.com/greglobinski" }
-  ]
+    siteTitle: "Brian Shea Hedberg's Personal Site", // <title>
+    shortSiteTitle: "Brian Hedberg's Personal Site", // <title> ending for posts and pages
+    siteDescription: "Thoughts, feelings, ponderings",
+    siteUrl: "https://briansheahedberg.com",
+    pathPrefix: "",
+    siteImage: "preview.jpg",
+    siteLanguage: "en",
+    // author
+    authorName: "brian shea hedberg",
+    authorTwitterAccount: "greglobinski",
+    // info
+    infoTitle: "brian shea hedberg",
+    infoTitleNote: "personal site",
+    // manifest.json
+    manifestName: "Personal Site - Brian Hedberg",
+    manifestShortName: "Personal Site", // max 12 characters
+    manifestStartUrl: "/",
+    manifestBackgroundColor: colors.background,
+    manifestThemeColor: colors.background,
+    manifestDisplay: "standalone",
+    // contact
+    contactEmail: "briansheahedberg@gmail.com",
+    // social
+    authorSocialLinks: [
+        { name: "github", url: "https://github.com/gemfarmer" }
+    ]
 };
